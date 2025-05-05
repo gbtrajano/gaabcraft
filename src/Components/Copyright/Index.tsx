e9af8a1f6afcb2ce193dd9317e29e57ea7415014
gaabrzx <gbtrajano1@gmail.com>
2025-05-05 17:45:28 -0300

    No Copyright

--- src/Components/Copyright/Index.tsx
+++
@@ -1,7 +0,0 @@
-export function Copyright() {
-  return (
-    <p className="flex items-center justify-center w-full text-xl text-white font-bold absolute bottom-4">
-      &copy; 2025 Gabriel
-    </p>
-  );
-}
